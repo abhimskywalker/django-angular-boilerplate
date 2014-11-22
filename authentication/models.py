14b76dfb696cdcb984c3dc11d0a10415aceac73b
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-22 19:08:34 +0530

    Created AccountManager

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,33 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
+
+        account = self.model(
+            email = self.normalize_email(email),
+            username = kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
 
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
@@ -16,8 +44,8 @@ class Account(AbstractBaseUser):
 
     objects = AccountManager()
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __unicode__(self):
         return self.email
