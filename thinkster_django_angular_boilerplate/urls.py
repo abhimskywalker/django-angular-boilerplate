46931497d3e3e8409f1954c8f1374a983ddd6aa3
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-23 14:40:58 +0530

    Create an API endpoint for LogoutView

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 
 from rest_framework import routers
 
-from authentication.views import AccountViewSet, LoginView
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -12,5 +12,6 @@ urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
     url(r"api/v1/auth/login/$", LoginView.as_view(), name="login"),
+    url(r"api/v1/auth/logout/$", LogoutView.as_view(), name="logout"),
     url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
