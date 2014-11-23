c6331f4b18c872c70a0f0ce3867f644a7a340dcd
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-23 12:03:57 +0530

    Configure AngularJS CSRF settings

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -6,11 +6,19 @@
             'thinkster.config',
             'thinkster.routes',
             'thinkster.authentication'
-        ]);
+        ])
+        .run(run);
 
     agular
         .module('thinkster.config', []);
 
     angular
         .module('thinkster.routes', ['ngRoute']);
+
+    run.$inject = ['$http'];
+
+    function run($http){
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
 })();
