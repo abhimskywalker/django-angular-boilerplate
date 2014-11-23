5dfc2fa3e949f03cbe4c284e9f36b6ce313409dc
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-23 19:24:42 +0530

    Make a new directive called posts in static/javascripts/posts/directives/posts.directive.js

---
+++ static/javascripts/posts/directives/posts.directive.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('thiknster.posts.directives')
+        .directive('posts', posts);
+
+    function posts(){
+        var directive = {
+            controller: 'PostsController',
+            controllerAs: 'vm',
+            restrict: 'E',
+            scope: {
+                posts: '='
+            },
+            templateUrl: 'static/templates/posts/posts.html'
+        };
+
+        return directive;
+    }
+})();
