1a1593ae194c11cb8260db5e07787becfa001a5b
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-23 17:51:53 +0530

    Make an API endpoint for the PostViewSet viewset and AccountPostsViewSet viewset

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,16 +1,24 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic.base import TemplateView
 
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet, LoginView, LogoutView
+from posts.views import PostViewSet, AccountPostsViewSet
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
+
+account_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+account_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(account_router.urls)),
     url(r"api/v1/auth/login/$", LoginView.as_view(), name="login"),
     url(r"api/v1/auth/logout/$", LogoutView.as_view(), name="logout"),
     url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
