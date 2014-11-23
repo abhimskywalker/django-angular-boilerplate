af6b7ec30c8b4b51a5f3b5c60f4cff576f8f64fe
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-23 13:02:36 +0530

    Make a view called LogoutView in authentication/views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from rest_framework import permissions, viewsets, status, views
 from rest_framework.response import Response
@@ -66,3 +66,12 @@ class LoginView(views.APIView):
                 'status': 'Unauthorized',
                 "error": "Looks like username and/or password is wrong."
             }, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated(),)
+
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
