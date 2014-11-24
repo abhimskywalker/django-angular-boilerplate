7848202757400ede9a3136e0e30acf305b8d559d
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-24 10:25:13 +0530

    Make a route for viewing user profiles

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -16,6 +16,10 @@
             controller: 'LoginController',
             controllerAs: 'vm',
             templateUrl: 'static/templates/authentication/login.html'
+        }).when('/+:username', {
+            controller: 'ProfileController',
+            controllerAs: 'vm',
+            templateUrl: '/static/templates/profiles/profile.html'
         }).when('/',{
             controller: 'IndexController',
             controllerAs: 'vm',
