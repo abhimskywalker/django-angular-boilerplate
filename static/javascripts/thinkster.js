20e159381e9b2d1fabad411bae300c9f4958bede
abhimskywalker <malik.abhishek.iitk@gmail.com>
2014-11-24 10:14:24 +0530

    Register thinkster.profiles as a dependency of the thinkster module

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -8,7 +8,8 @@
             'thinkster.authentication',
             'thinkster.layout',
             'thinkster.posts',
-            'thinkster.utils'
+            'thinkster.utils',
+            'thinkster.profiles'
         ])
         .run(run);
 
